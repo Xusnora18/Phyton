4f44c5aca4dd593bc2d7445fbfeed5717dcce797
Nargiza <xasanovanargiza1504@gmail.com>
2025-05-22 23:02:28 +0500

    Add files via upload

---
+++ lesson-2/homework/python-2Hw.py
@@ -0,0 +1,14 @@
+Python 3.12.3 (tags/v3.12.3:f6650f9, Apr  9 2024, 14:05:25) [MSC v.1938 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Users/user/AppData/Local/Programs/Python/Python312/python-2Hw.py =
+1. Age Calculator: Alice, you are 25 years old.
+2. Extract Car Names 1: Malibu
+3. Extract Car Names 2: Matiz
+4. Extract Residence Area: London
+5. Reverse String: olleh
+6. Count Vowels: 6
+7. Find Maximum Value: 99
+8. Check Palindrome: True
+9. Extract Email Domain: domain.com
+10. Generate Random Password: jArRo&~B6|!D
